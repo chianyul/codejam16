b3171f91879602cad8d90e455a8d1f5ef1d06199
Chianyu Liu <Chianyu Liu>
2016-10-31 03:02:06 -0400

    created Google app script of form. known issue: required doesn't work for radio questions

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 function doGet(e) {
-	return HtmlService.createHtmlOutputFromFile('forms.html').setTitle("CodeJam 2016 Registration");
+	return HtmlService.createHtmlOutputFromFile('form.html').setTitle("CodeJam 2016 Registration");
 }
 
-function uploadFileToGoogleDrive(data, file, name, email) {
-	
+function uploadFileToGoogleDrive(data, file, fname, lname) {	
 	try {
-		var dropbox = "Received Files";
+		var dropbox = "CodeJam Participant Resumes";
 		var folder, folders = DriveApp.getFoldersByName(dropbox);
 	
 		if (folders.hasNext()) {
@@ -17,7 +16,7 @@ function uploadFileToGoogleDrive(data, file, name, email) {
 		var contentType = data.substring(5,data.indexOf(';')),
 		bytes = Utilities.base64Decode(data.substr(data.indexOf('base64,')+7)),
 		blob = Utilities.newBlob(bytes, contentType, file),
-		file = folder.createFolder([name, email].join(" ")).createFile(blob);
+		file = folder.createFolder([fname, lname].join(" ")).createFile(blob);
 	
 		return "OK";
 	
@@ -26,25 +25,8 @@ function uploadFileToGoogleDrive(data, file, name, email) {
 	}
 }
 
-function assignEditUrls() {
-	var form = FormApp.openById('yourFormKey');
-		//enter form ID here
-
-	var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('yourWorksheetName');
-
-		//Change the sheet name as appropriate
-	var data = sheet.getDataRange().getValues();
-	var urlCol = ; // column number where URL's should be populated; A = 1, B = 2 etc
-	var responses = form.getResponses();
-	var timestamps = [], urls = [], resultUrls = [];
-	
-	for (var i = 0; i < responses.length; i++) {
-		timestamps.push(responses[i].getTimestamp().setMilliseconds(0));
-		urls.push(responses[i].getEditResponseUrl());
-	}
-	for (var j = 1; j < data.length; j++) {
-
-		resultUrls.push([data[j][0]?urls[timestamps.indexOf(data[j][0].setMilliseconds(0))]:'']);
-	}
-	sheet.getRange(2, urlCol, resultUrls.length).setValues(resultUrls);
+function addResponseToSheet(q0, q1, q2, q3, q4, q5, q6, q7, q8, q9) {
+	var mySheet = SpreadsheetApp.openById("1FA4eRtrpiNYj7bi2UUYS7Byxmon4iNAMAEdeV53YUSI").getSheets()[0];
+	var timestamp = new Date();
+	mySheet.appendRow([timestamp, q0, q1, q2, q3, q4, q5, q6, q7, q8, q9]);
 }
